35b90b7a42ea55b03430d840b8325036257c00eb
Kirill <semerenkok@rambler.ru>
2022-08-16 15:17:37 +0300

    1

--- conroller/controller.go
+++ conroller/controller.go
@@ -12,7 +12,8 @@ func Server() {
 	r := mux.NewRouter()
 	r.Host("http://localhost:8080")
 	r.HandleFunc("/api", handleConnection)
-	r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
+	//r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
+	http.Handle("/web/", http.FileServer(http.Dir("./web")))
 	r.HandleFunc("/", homePage)
 
 	err := http.ListenAndServe("localhost:8080", r)
